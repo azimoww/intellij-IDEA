5ed6fcc21fe7be4704dbb825197ae3a901673dea
azimoww <heyderezimov@gmail.com>
2025-10-03 20:08:05 +0400

    Update SetsAndMapsExample.java

--- SetExamples/SetsAndMapsExample.java
+++ SetExamples/SetsAndMapsExample.java
@@ -1,16 +1,15 @@
-package SetExamples;
 import java.util.*;
 
-public class SetsAndMapsExample {
+public class SetsExample {
     public static void main(String[] args) {
-
+ 
         HashSet<String> cities = new HashSet<>();
         cities.add("Baku");
         cities.add("London");
         cities.add("Paris");
         cities.add("Berlin");
         cities.add("Rome");
-        boolean added = cities.add("Baku");
+        boolean added = cities.add("Baku"); 
         System.out.println("HashSet: " + cities);
         System.out.println("Попытка добавить дубликат: " + added);
 
@@ -32,39 +31,19 @@ public class SetsAndMapsExample {
         numbers.add(20);
         System.out.println("TreeSet (отсортировано): " + numbers);
 
+
         NavigableSet<Integer> navSet = new TreeSet<>(numbers);
         System.out.println("NavigableSet: " + navSet);
-        System.out.println("higher(30): " + navSet.higher(30));
-        System.out.println("floor(25): " + navSet.floor(25));
-        System.out.println("descendingSet: " + navSet.descendingSet());
+        System.out.println("higher(30): " + navSet.higher(30));  
+        System.out.println("floor(25): " + navSet.floor(25));     
+        System.out.println("descendingSet: " + navSet.descendingSet()); 
 
-
+     
         HashMap<String, Integer> grades = new HashMap<>();
         grades.put("Ali", 90);
         grades.put("Nigar", 85);
         grades.put("Murad", 70);
         System.out.println("HashMap (оценки студентов): " + grades);
-
-        
-        grades.put("Ali", 95);
-        System.out.println("После обновления: " + grades);
-
-        LinkedHashMap<String, Integer> linkedGrades = new LinkedHashMap<>();
-        linkedGrades.put("Ali", 90);
-        linkedGrades.put("Nigar", 85);
-        linkedGrades.put("Murad", 70);
-        System.out.println("LinkedHashMap (порядок добавления): " + linkedGrades);
-
-
-        TreeMap<Integer, String> students = new TreeMap<>();
-        students.put(3, "Ali");
-        students.put(1, "Nigar");
-        students.put(2, "Murad");
-        System.out.println("TreeMap (отсортированные ключи): " + students);
-
-        System.out.println("firstEntry: " + students.firstEntry());
-        System.out.println("lastEntry: " + students.lastEntry());
-        System.out.println("higherEntry(1): " + students.higherEntry(1));
     }
 }
 
